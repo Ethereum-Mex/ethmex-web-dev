acaaa6e523470b0e0c68b6849c12b299a1defbc5
Liz Durán <lizduran@Lizs-MacBook-Air.local>
2025-04-23 09:14:40 -0600

    FAQ EM description changed

--- src/components/NuestrosValores/NuestrosValores.js
+++ src/components/NuestrosValores/NuestrosValores.js
@@ -6,7 +6,7 @@ const valores = [
     { id: 'inclusion', titulo: 'Equidad e Inclusión', descripcion: 'Creemos en la diversidad de perspectivas y en la importancia de construir una organización accesible para las personas, sin importar su nivel de experiencia, género, ubicación o contexto.' },
     { id: 'educacion', titulo: 'Educación', descripcion: 'Estamos comprometidos con el fortalecimiento del ecosistema Ethereum a través de eventos anuales e iniciativas educativas.' },
     { id: 'innovacion', titulo: 'Innovación', descripcion: 'Nos mantenemos en constante evolución, adaptándonos a los cambios del ecosistema Web3.' },
-    { id: 'transparencia', titulo: 'Transparencia', descripcion: 'La transparencia es fundamental en todo lo que hacemos. Los recursos obtenidos se destinan exclusivamente a beneficiar a la comunidad y siempre será de conocimiento público.' },
+    { id: 'transparencia', titulo: 'Transparencia', descripcion: 'La transparencia es fundamental en todo lo que hacemos. Los recursos obtenidos se destinan principalmente para crear impacto en la comunidad Web3' },
     { id: 'comunidad', titulo: 'Comunidad (Infinite Gardeners 🌱)', descripcion: 'Crecemos juntos  e impulsamos a otros a crecer. Nos enfocamos en fomentar un ecosistema descentralizado donde cada persona pueda contribuir, aprender y mejorar continuamente.' }
 ];
 
